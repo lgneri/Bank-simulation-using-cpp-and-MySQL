e27f9dfb3c1e7a49989422806b9949bc4ec23e8c
lgneri <101299486+lgneri@users.noreply.github.com>
2023-12-05 19:15:38 -0300

    Update Database.h

--- Database.h
+++ Database.h
@@ -1,3 +1,6 @@
+/** Arquivo Database.h. Trabalho final EEL670 2022.2
+*   Aluno: Luis Guilherme Neri Ferreira */
+
 #ifndef DATABASE_H
 #define DATABASE_H
 
@@ -16,17 +19,20 @@ class Database
         const char* password = "senhaPublica";
         const char* database = "neriBank";
         const float interest = 1.02;
+        MYSQL_RES* performQuery(MYSQL*, const char*);
+        void modify(MYSQL*, int, const char*, float);
     public:
         MYSQL* connect();
-        MYSQL_RES* performQuery(MYSQL*, const char*);
         Database();
         void addClient(MYSQL*, Client);
         bool clientExists(MYSQL*, int);
         bool correctPassword(MYSQL*, int, const char*);
         map<string, char*> getClientInfo(MYSQL*, int);
-        void modify(MYSQL*, int, const char*, float);
         void cashTransaction(MYSQL*, int, float);
         void pix(MYSQL*, int, int, float);
+        void loan(MYSQL*, int, float);
+        void payLoan(MYSQL*, int);
+        void simulateMonth(MYSQL*);
 };
 
 #endif
